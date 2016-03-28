758e9e92592803f1c66ab0fd9c8b71fbfa98022e
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2016-03-28 16:17:56 -0400

    console running

--- metrics/flesch.py
+++ metrics/flesch.py
@@ -6,18 +6,22 @@ not_punctuation = lambda w: not (len(w)==1 and (not w.isalpha()))
 get_word_count = lambda text: len(filter(not_punctuation, word_tokenize(text)))
 get_sent_count = lambda text: len(sent_tokenize(text))
 
-# Flesch reading ease
-# reference: https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests
-def flesch(text):
+def text_statistics(text):
     word_count = get_word_count(text)
     sent_count = get_sent_count(text)
     syllable_count = sum(map(lambda w: max(numsyllables(w)), word_tokenize(text)))
-    return 206.835 - 1.015*word_count/sent_count - 84.6*syllable_count/word_count
+    return word_count, sent_count, syllable_count
+
+# Flesch reading ease
+# reference: https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests
+flesch_formula = lambda word_count, sent_count, syllable_count : 206.835 - 1.015*word_count/sent_count - 84.6*syllable_count/word_count
+def flesch(text):
+    word_count, sent_count, syllable_count = text_statistics(text)
+    return flesch_formula(word_count, sent_count, syllable_count)
 
 # Flesch-Kincaid grade level
 # reference: https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests
+fk_formula = lambda word_count, sent_count, syllable_count : 0.39 * word_count / sent_count + 11.8 * syllable_count / word_count - 15.59
 def flesch_kincaid(text):
-    word_count = get_word_count(text)
-    sent_count = get_sent_count(text)
-    syllable_count = sum(map(lambda w: max(numsyllables(w)), word_tokenize(text)))
-    return 0.39 * word_count / sent_count + 11.8 * syllable_count / word_count - 15.59
+    word_count, sent_count, syllable_count = text_statistics(text)
+    return fk_formula(word_count, sent_count, syllable_count)
---
+++ score_readability.py
@@ -0,0 +1,21 @@
+import argparse
+import metrics.flesch as f
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description='Calculate the readability indices of the given text documents.')
+    parser.add_argument('filepaths', nargs='+', help='paths of the text files')
+    return parser
+
+if __name__ == '__main__':
+    parser = get_argparser()
+    args = parser.parse_args()
+
+    for filepath in args.filepaths:
+        text = open(filepath, 'rb').read()
+        print 'Filepath: ', filepath
+        word_count, sent_count, syllable_count = f.text_statistics(text)
+        print 'Word count = ', word_count
+        print 'Sentence count = ', sent_count
+        print 'Syllable count = ', syllable_count
+        print 'Flesch readability ease = ', f.flesch_formula(word_count, sent_count, syllable_count)
+        print 'Flesch-Kincaid grade level = ', f.fk_formula(word_count, sent_count, syllable_count)
