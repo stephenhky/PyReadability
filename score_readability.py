18eb980ae7c4a1fe73f25393e03239fc413af361
stephenhky <cwin31>
2016-09-27 23:31:04 -0400

    consider UTF-8 encoding

--- score_readability.py
+++ score_readability.py
@@ -4,6 +4,7 @@ import metrics.flesch as f
 def get_argparser():
     parser = argparse.ArgumentParser(description='Calculate the readability indices of the given text documents.')
     parser.add_argument('filepaths', nargs='+', help='paths of the text files.')
+    parser.add_argument('--utf8', action='store_true', default=False, help='use UTF-8 encoding.')
     return parser
 
 if __name__ == '__main__':
@@ -11,7 +12,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     for filepath in args.filepaths:
-        text = open(filepath, 'rb').read()
+        text = open(filepath, 'rb').read() if not args.utf8 else open(filepath, 'r').read().decode('utf-8')
         print 'Filepath: ', filepath
         word_count, sent_count, syllable_count = f.text_statistics(text)
         print 'Word count = ', word_count
